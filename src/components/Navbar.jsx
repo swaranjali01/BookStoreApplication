184f8fd2cde7f3dff283caaef00a77dc9a01be8f
swaranjali01 <swaragawande26@gmail.com>
2024-10-24 20:48:45 +0530

    change in nav bar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,22 @@
 import React from 'react'
 
 export default function Navbar() {
+  const navItems =(
+<>
+<li>
+        <a>Home</a>
+      </li>
+      <li>
+        <a>Course</a>
+      </li>
+      <li>
+        <a>Contact</a>
+      </li>
+      <li>
+        <a>About</a>
+      </li>
+</>
+);
   return (
    <>
    <div>
@@ -24,40 +40,19 @@ export default function Navbar() {
       <ul
         tabIndex={0}
         className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
-        <li><a>Item 1</a></li>
-        <li>
-          <a>Parent</a>
-          <ul className="p-2">
-            <li><a>Submenu 1</a></li>
-            <li><a>Submenu 2</a></li>
-          </ul>
-        </li>
-        <li><a>Item 3</a></li>
+        {navItems}
       </ul>
     </div>
+    <div className="navbar-end">
     <a className="text-2xl font-bold cursor-pointer">BookStore</a>
   </div>
   <div className="navbar-center hidden lg:flex">
-    <ul className="menu menu-horizontal px-1">
-      <li>
-        <a>Home</a>
-      </li>
-      <li>
-        <a>Course</a>
-      </li>
-      <li>
-        <a>Contact</a>
-      </li>
-      <li>
-        <a>About</a>
-      </li>
-      
-      
-    </ul>
+    <ul className="menu menu-horizontal px-1">{navItems}</ul>
   </div>
   <div className="navbar-end">
     <a className="btn">Button</a>
   </div>
+</div>
 </div>
    </div>
    </>
