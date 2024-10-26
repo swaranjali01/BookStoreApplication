b63a37facbb54314dfd5a24289274d758895ce0f
swaranjali01 <swaragawande26@gmail.com>
2024-10-27 01:07:57 +0530

    update navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -66,7 +66,7 @@ export default function Navbar() {
 </label>
   </div>
   <div className="">
-    <a className="btn">Login</a>
+    <a className="bg-black text-white px-2 py-2 rounded-md hover:bg-slate-800 duration-30 cursor-pointer">Login</a>
   </div>
 </div>
 </div>
