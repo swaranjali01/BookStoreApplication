5c1444cfe4d17ffdbd997308ac7cd31dcd30a551
swaranjali01 <swaragawande26@gmail.com>
2024-10-28 19:39:40 +0530

    home page update

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
-import React from 'react'
-import Navbar from './components/Navbar'
+import React from 'react';
+import Navbar from './components/Navbar';
+import Banner from './components/Banner';
 
 export default function () {
   return (
--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,14 +1,27 @@
-import React from 'react'
+import React from 'react';
 
 function Banner() {
   return (
     <>
-    <div className="max-w-screen-2xl container mx-auto md:px-20 px-4 flex">
-      <div className='w-full md:w-1/2'>Left</div>
+    <div className="max-w-screen-2xl container mx-auto md:px-20 px-4 flex flex-col md:flex-row">
+      <div className='w-full md:w-1/2 mt-32'>
+      <div className='space-y-12'>
+      <h1 className='text-4xl font-bold'>
+        Hello, welcome here to learn something{" "}
+        <span className='text-pink-500'>new everyday!!!</span>
+      </h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+        Quas ipsam in obcaecati ipsum illum consequuntur quaerat 
+        dolore porro eveniet sed harum, ut sunt, hic nulla 
+        deleniti vero cum omnis doloremque.
+      </p>
+      </div>
+      </div>
       <div className='w-full md:w-1/2'>Right</div>
     </div>
     </>
   )
 }
 
-export default Banner
+export default Banner;
