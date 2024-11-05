69ac953167890bc1cc383b3567d59af44bdd54a9
swaranjali01 <swaragawande26@gmail.com>
2024-11-05 23:49:45 +0530

    updatede  ....

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -8,7 +8,19 @@ function Cards({ item }) {
             <figure>
                 <img src={item.image} alt="shoes" />
             </figure>
-
+            <div className="card-body">
+            <h2 className="card-title">
+              {item.name}
+              <div className="badge badge-secondary">{item.category}</div>
+            </h2>
+            <p>{item.title}</p>
+            <div className="card-actions justify-between">
+              <div className="badge badge-outline">${item.price}</div>
+              <div className=" cursor-pointer px-2 py-1 rounded-full border-[2px] hover:bg-pink-500 hover:text-white duration-200">
+                Buy Now
+              </div>
+            </div>
+          </div>
         </div>
     </div>
     </>
