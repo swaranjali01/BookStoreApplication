406f01be5f6a90652316e72db2bed838dd1c486e
swaranjali01 <swaragawande26@gmail.com>
2024-11-05 23:23:21 +0530

    cards pg created ....

---
+++ src/components/Cards.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Cards({ item }) {
+  return (
+    <>
+    <div>
+        <div className="">
+
+        </div>
+    </div>
+    </>
+    
+  )
+}
+
+export default Cards
