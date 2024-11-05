3636cbaab465caa4bb2f5265c8da94be704ad046
swaranjali01 <swaragawande26@gmail.com>
2024-11-05 23:25:26 +0530

    updatede  ....

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -4,7 +4,10 @@ function Cards({ item }) {
   return (
     <>
     <div>
-        <div className="">
+        <div className=" card w-96 bg-base-100 shadow-xl">
+            <figure>
+                <img src={item.image} alt="shoes" />
+            </figure>
 
         </div>
     </div>
