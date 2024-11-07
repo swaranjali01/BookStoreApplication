dc6f760d4bb24a55a1210649c58cf9802b84119e
swaranjali01 <swaragawande26@gmail.com>
2024-11-07 21:48:24 +0530

    update in navbar

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,8 +4,8 @@ function Footer() {
   return (
     <div>
       <hr />
-      <footer className="footer footer-center p-10 text-base-content rounded">
-        <nav className="grid grid-flow-col gap-4">
+      <footer className="footer footer-center p-10 text-base-content rounded dark:bg-slate-900 dark:text-white">
+          <nav className="grid grid-flow-col gap-4">
           <a className="link link-hover">About us</a>
           <a className="link link-hover">Contact</a>
           <a className="link link-hover">Jobs</a>
